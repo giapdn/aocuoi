fa1817514d82c7a2555f637afa9e6dbcef591b00
giapdn <donguyengiapicloud@gmail.com>
2024-01-23 23:46:02 +0700

    heheh

--- admin/Controllers/BaidangController.php
+++ admin/Controllers/BaidangController.php
@@ -1,66 +1,75 @@
-<?php 
+<?php
 
 require_once "Models/baidang.php";
-class BaiDangController{
-    
+class BaiDangController
+{
+
     protected $use;
     protected $baidang;
-    public function __construct(){
-        $this->use= new khachhang;
+    public function __construct()
+    {
+        $this->use = new khachhang;
         $this->baidang = new baidang;
-
     }
     //bài đăng
-    public function ListBaidang(){
+    public function ListBaidang()
+    {
         $baidang = $this->baidang->AllBaidang();
-        include_once "Views/baiviet/list.php"; 
+        include_once "Views/baiviet/list.php";
     }
-    public function AddBaidang(){
-        $listusername = $this->use ->getAllusername();
-        if(isset($_POST['them'])){
-            $tieu_de=$_POST['tieu_de'];
-            $noi_dung=$_POST['noi_dung'];
-            $path=$_FILES['path']['name'];
-            $target_dir = "uploads/";
-            $target_file = $target_dir . basename($_FILES["path"]["name"]);
-            move_uploaded_file($_FILES["path"]["tmp_name"], $target_file);
-            $username=$_POST['username'];
-            $check =$this->baidang->thembaiviet($tieu_de,$noi_dung,$path,$username);
-            if(!$check){
-                echo '<script>alert("them thành công")</script>';
-            echo '<script>window.location.href="../admin/index.php?url=list-baiviet"</script>';
-            }
-        }
-        include_once "Views/baiviet/add.php";
-    }
-    public function deleteBaiViet(){
-        if(isset($_GET['id']) && $_GET['id']){
-            $id= $_GET['id'];
-            $this->baidang->xoaBaiViet($id);
-            echo '<script>alert("Xóa thành công")</script>';
-            echo '<script>window.location.href="../admin/index.php?url=list-baiviet"</script>';
-        }
-    }
-    public function EditBaiViet(){
-        $listusername = $this->use ->getAllusername();
-        if(isset($_GET['id']) && $_GET['id']){
-            $id= $_GET['id'];
-            $hienThiBaiViet = $this->baidang->hienthi($id);
-        }
-        include_once "Views/baiviet/sua.php";
-        if(isset($_POST['sua'])){
-            $id= $_POST['id_bai_dang'];
-            $tieu_de= $_POST['tieu_de'];
-            $noi_dung= $_POST['noi_dung'];
+    public function AddBaidang()
+    {
+        $listusername = $this->use->getAllusername();
+        if (isset($_POST['them'])) {
+            $tieu_de = $_POST['tieu_de'];
+            $noi_dung = $_POST['noi_dung'];
             $path = $_FILES['path']['name'];
             $target_dir = "uploads/";
             $target_file = $target_dir . basename($_FILES["path"]["name"]);
             move_uploaded_file($_FILES["path"]["tmp_name"], $target_file);
             $username = $_POST['username'];
-            $check = $this->baidang->suaBaiViet($id,$tieu_de,$noi_dung,$path,$username);
-            if(!$check){
-                echo '<script>alert("sửa thành công")</script>';
+            $check = $this->baidang->thembaiviet($tieu_de, $noi_dung, $path, $username);
+            if (!$check) {
+                echo '<script>alert("them thành công")</script>';
                 echo '<script>window.location.href="../admin/index.php?url=list-baiviet"</script>';
+            }
+        }
+        include_once "Views/baiviet/add.php";
+    }
+    public function deleteBaiViet()
+    {
+        if (isset($_GET['id']) && $_GET['id']) {
+            $id = $_GET['id'];
+            $this->baidang->xoaBaiViet($id);
+            echo '<script>alert("Xóa thành công")</script>';
+            echo '<script>window.location.href="../admin/index.php?url=list-baiviet"</script>';
+        }
+    }
+    public function EditBaiViet()
+    {
+        $listusername = $this->use->getAllusername();
+        if (isset($_POST['sua'])) {
+        }
+
+        if ($_GET["url"] == "sua-baiviet") {
+            if (isset($_POST["id_bai_dang"])) {
+                $id = $_POST['id_bai_dang'];
+                $tieu_de = $_POST['tieu_de'];
+                $noi_dung = $_POST['noi_dung'];
+                $path = $_FILES['path']['name'];
+                $target_dir = "uploads/";
+                $target_file = $target_dir . basename($_FILES["path"]["name"]);
+                move_uploaded_file($_FILES["path"]["tmp_name"], $target_file);
+                $username = $_POST['username'];
+                $check = $this->baidang->suaBaiViet($id, $tieu_de, $noi_dung, $path, $username);
+                if (!$check) {
+                    echo '<script>alert("sửa thành công")</script>';
+                    echo '<script>window.location.href="../admin/index.php?url=list-baiviet"</script>';
+                }
+            } else {
+                $id = $_GET['id'];
+                $hienThiBaiViet = $this->baidang->hienthi($id);
+                require_once "Views/baiviet/sua.php";
             }
         }
     }
@@ -78,6 +87,3 @@ class BaiDangController{
     //     }
     // }
 }
-
-
-?>
--- admin/Controllers/CategoryController.php
+++ admin/Controllers/CategoryController.php
@@ -4,55 +4,57 @@ class CategoryController
 {
     //danh mục
     protected $category;
-    public function __construct(){
-        $this->category= new Category;
-
+    public function __construct()
+    {
+        $this->category = new Category;
     }
     public function ListCategory()
     {
         $danhmuc = $this->category->AllCategory();
         include_once "Views/Category/list.php";
     }
-    public function AddCategory(){
-        if(isset($_POST['them'])){
+    public function AddCategory()
+    {
+        if (isset($_POST['them'])) {
             $mota = $_POST['mo_ta'];
             $path = $_FILES['path']['name'];
             $target_dir = "uploads/";
             $target_file = $target_dir . basename($_FILES["path"]["name"]);
             move_uploaded_file($_FILES["path"]["tmp_name"], $target_file);
-            $this->category->themCategory($mota,$path);
+            $this->category->themCategory($mota, $path);
             echo '<script>alert("Thêm danh mục thành công")</script>';
             echo '<script>window.location.href="../admin/index.php?url=list-category"</script>';
         }
         include_once "Views/Category/add.php";
     }
-    public function EditCategory(){
-        if(isset($_GET['id'])&&($_GET['id'])){
-            $id=$_GET['id'];
-            $hienthiCategory = $this->category->hienthiCategory($id);
-        }
-        include_once "Views/Category/edit.php";
-        if(isset($_POST['sua'])){ 
-            $id=$_POST['id_danh_muc'];
-            $mota =$_POST['mo_ta'];
-            $path = $_FILES['path']['name'];
-            $target_dir = "uploads/";
-            $target_file = $target_dir . basename($_FILES["path"]["name"]);
-            move_uploaded_file($_FILES["path"]["tmp_name"], $target_file);
-            $this->category->suaCategory($id,$mota,$path);
-            echo '<script>alert("sửa thành công")</script>';
-            echo '<script>window.location.href="../admin/index.php?url=list-category"</script>';
+    public function EditCategory()
+    {
+        if ($_GET["url"] == "sua-category") {
+            if (isset($_POST["mo_ta"])) {
+                $id = $_POST['id_danh_muc'];
+                $mota = $_POST['mo_ta'];
+                $path = $_FILES['path']['name'];
+                $target_dir = "uploads/";
+                $target_file = $target_dir . basename($_FILES["path"]["name"]);
+                move_uploaded_file($_FILES["path"]["tmp_name"], $target_file);
+                $this->category->suaCategory($id, $mota, $path);
+                echo '<script>alert("sửa thành công")</script>';
+                echo '<script>window.location.href="../admin/index.php?url=list-category"</script>';
+            } else {
+                $id = $_GET['id'];
+                $hienthiCategory = $this->category->hienthiCategory($id);
+                require_once "Views/Category/edit.php";
+            }
         }
     }
-    
-    public function deleteCategory(){
-        if(isset($_GET['id']) && $_GET['id']){
-            $id= $_GET['id'];
+
+    public function deleteCategory()
+    {
+        if (isset($_GET['id']) && $_GET['id']) {
+            $id = $_GET['id'];
             $this->category->xoaCategory($id);
             echo '<script>alert("Xóa thành công")</script>';
             echo '<script>window.location.href="../admin/index.php?url=list-category"</script>';
         }
     }
-
-
 }
