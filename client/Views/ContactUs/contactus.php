2633aa79c295d7afd5f77a70337a21d878d0d618
giapdn <donguyengiapicloud@gmail.com>
2024-01-24 18:59:59 +0700

    new update

---
+++ client/Views/ContactUs/contactus.php
@@ -0,0 +1,239 @@
+<div class="offcanvas-menu_wrapper" id="offcanvasMenu">
+    <div class="offcanvas-menu-inner">
+        <a href="#" class="btn-close"><i class="ion-android-close"></i></a>
+        <div class="offcanvas-inner_logo">
+            <a href="shop-left-sidebar.html">
+                <img src="client/assets/images/menu/logo/1.png" alt="Munoz's Offcanvas Logo">
+            </a>
+        </div>
+        <div class="short-desc">
+            <p>We are a team of designers and developers that create high quality HTML Template &
+                Woocommerce,
+                Shopify Themes.
+            </p>
+        </div>
+        <div class="offcanvas-component first-child">
+            <span class="offcanvas-component_title">Currency</span>
+            <ul class="offcanvas-component_menu">
+                <li><a href="javascript:void(0)">EUR</a></li>
+                <li><a href="javascript:void(0)">GBP</a></li>
+                <li class="active"><a href="javascript:void(0)">USD</a></li>
+            </ul>
+        </div>
+        <div class="offcanvas-component">
+            <span class="offcanvas-component_title">Language</span>
+            <ul class="offcanvas-component_menu">
+                <li class="active"><a href="javascript:void(0)">English</a></li>
+                <li><a href="javascript:void(0)">French</a></li>
+            </ul>
+        </div>
+        <div class="offcanvas-component">
+            <span class="offcanvas-component_title">My Account</span>
+            <ul class="offcanvas-component_menu">
+                <li><a href="my-account.html">Register</a></li>
+                <li><a href="login-register.html">Login</a></li>
+            </ul>
+        </div>
+        <div class="offcanvas-inner-social_link">
+            <div class="kenne-social_link">
+                <ul>
+                    <li class="facebook">
+                        <a href="https://www.facebook.com/" data-bs-toggle="tooltip" target="_blank" title="Facebook">
+                            <i class="fab fa-facebook"></i>
+                        </a>
+                    </li>
+                    <li class="twitter">
+                        <a href="https://twitter.com/" data-bs-toggle="tooltip" target="_blank" title="Twitter">
+                            <i class="fab fa-twitter-square"></i>
+                        </a>
+                    </li>
+                    <li class="youtube">
+                        <a href="https://www.youtube.com/" data-bs-toggle="tooltip" target="_blank" title="Youtube">
+                            <i class="fab fa-youtube"></i>
+                        </a>
+                    </li>
+                    <li class="google-plus">
+                        <a href="https://www.plus.google.com/discover" data-bs-toggle="tooltip" target="_blank"
+                            title="Google Plus">
+                            <i class="fab fa-google-plus"></i>
+                        </a>
+                    </li>
+                    <li class="instagram">
+                        <a href="https://rss.com/" data-bs-toggle="tooltip" target="_blank" title="Instagram">
+                            <i class="fab fa-instagram"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="offcanvas-search_wrapper" id="searchBar">
+    <div class="offcanvas-menu-inner">
+        <div class="container">
+            <a href="#" class="btn-close"><i class="ion-android-close"></i></a>
+            <!-- Begin Offcanvas Search Area -->
+            <div class="offcanvas-search">
+                <form action="#" class="hm-searchbox">
+                    <input type="text" placeholder="Search for item...">
+                    <button class="search_btn" type="submit"><i class="ion-ios-search-strong"></i></button>
+                </form>
+            </div>
+            <!-- Offcanvas Search Area End Here -->
+        </div>
+    </div>
+</div>
+<div class="global-overlay"></div>
+</header>
+<!-- Main Header Area End Here Two -->
+
+<!-- Begin Kenne's Breadcrumb Area -->
+<div class="breadcrumb-area">
+    <div class="container">
+        <div class="breadcrumb-content">
+            <h2>Other</h2>
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li class="active">Contact</li>
+            </ul>
+        </div>
+    </div>
+</div>
+<!-- Kenne's Breadcrumb Area End Here -->
+<!-- Begin Contact Main Page Area -->
+<div class="contact-main-page">
+    <div class="container">
+        <div id="google-map"></div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-5 offset-lg-1 col-md-12 order-1 order-lg-2">
+                <div class="contact-page-side-content">
+                    <h3 class="contact-page-title">Contact Us</h3>
+                    <p class="contact-page-message">Claritas est etiam processus dynamicus, qui sequitur
+                        mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum
+                        claram anteposuerit litterarum formas human.</p>
+                    <div class="single-contact-block">
+                        <h4><i class="fa fa-fax"></i> Address</h4>
+                        <p>123 Main Street, Anytown, CA 12345 – USA</p>
+                    </div>
+                    <div class="single-contact-block">
+                        <h4><i class="fa fa-phone"></i> Phone</h4>
+                        <p>Mobile: (08) 123 456 789</p>
+                        <p>Hotline: 1009 678 456</p>
+                    </div>
+                    <div class="single-contact-block last-child">
+                        <h4><i class="fa fa-envelope-o"></i> Email</h4>
+                        <p>yourmail@domain.com</p>
+                        <p>support@hastech.company</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-12 order-2 order-lg-1">
+                <div class="contact-form-content">
+                    <h3 class="contact-page-title">Tell Us Your Message</h3>
+                    <div class="contact-form">
+                        <form id="contact-form" action="https://whizthemes.com/mail-php/mamunur/kenne/kenne.php">
+                            <div class="form-group">
+                                <label>Your Name <span class="required">*</span></label>
+                                <input type="text" name="con_name" id="con_name" required>
+                            </div>
+                            <div class="form-group">
+                                <label>Your Email <span class="required">*</span></label>
+                                <input type="email" name="con_email" id="con_email" required>
+                            </div>
+                            <div class="form-group">
+                                <label>Subject</label>
+                                <input type="text" name="con_subject" id="con_subject">
+                            </div>
+                            <div class="form-group form-group-2">
+                                <label>Your Message</label>
+                                <textarea name="con_message" id="con_message"></textarea>
+                            </div>
+                            <div class="form-group">
+                                <button type="submit" value="submit" id="submit" class="kenne-contact-form_btn"
+                                    name="submit">send</button>
+                            </div>
+                        </form>
+                    </div>
+                    <p class="form-message"></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Contact Main Page Area End Here -->
+
+<!-- Begin Brand Area -->
+<div class="brand-area ">
+    <div class="container">
+        <div class="brand-nav border-top ">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="kenne-element-carousel brand-slider slider-nav" data-slick-options='{
+                                "slidesToShow": 6,
+                                "slidesToScroll": 1,
+                                "infinite": false,
+                                "arrows": false,
+                                "dots": false,
+                                "spaceBetween": 30
+                                }' data-slick-responsive='[
+                                {"breakpoint":992, "settings": {
+                                "slidesToShow": 4
+                                }},
+                                {"breakpoint":768, "settings": {
+                                "slidesToShow": 3
+                                }},
+                                {"breakpoint":576, "settings": {
+                                "slidesToShow": 2
+                                }}
+                            ]'>
+
+                        <div class="brand-item">
+                            <a href="javascript:void(0)">
+                                <img src="client/assets/images/brand/1.png" alt="Brand Images">
+                            </a>
+                        </div>
+                        <div class="brand-item">
+                            <a href="javascript:void(0)">
+                                <img src="client/assets/images/brand/2.png" alt="Brand Images">
+                            </a>
+                        </div>
+                        <div class="brand-item">
+                            <a href="javascript:void(0)">
+                                <img src="client/assets/images/brand/3.png" alt="Brand Images">
+                            </a>
+                        </div>
+                        <div class="brand-item">
+                            <a href="javascript:void(0)">
+                                <img src="client/assets/images/brand/4.png" alt="Brand Images">
+                            </a>
+                        </div>
+                        <div class="brand-item">
+                            <a href="javascript:void(0)">
+                                <img src="client/assets/images/brand/5.png" alt="Brand Images">
+                            </a>
+                        </div>
+                        <div class="brand-item">
+                            <a href="javascript:void(0)">
+                                <img src="client/assets/images/brand/6.png" alt="Brand Images">
+                            </a>
+                        </div>
+                        <div class="brand-item">
+                            <a href="javascript:void(0)">
+                                <img src="client/assets/images/brand/1.png" alt="Brand Images">
+                            </a>
+                        </div>
+                        <div class="brand-item">
+                            <a href="javascript:void(0)">
+                                <img src="client/assets/images/brand/2.png" alt="Brand Images">
+                            </a>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Brand Area End Here -->
