425c28fdf28331f6529cd8afc1bb60e0e7711462
mod30 <lann47897@gmail.com>
2024-01-25 19:44:54 +0700

    moden

--- admin/Controllers/CategoryController.php
+++ admin/Controllers/CategoryController.php
@@ -21,9 +21,11 @@ class CategoryController
             $target_dir = "uploads/";
             $target_file = $target_dir . basename($_FILES["path"]["name"]);
             move_uploaded_file($_FILES["path"]["tmp_name"], $target_file);
-            $this->category->themCategory($mota, $path);
-            echo '<script>alert("Thêm danh mục thành công")</script>';
-            echo '<script>window.location.href="../admin/index.php?url=list-category"</script>';
+            $check =$this->category->themCategory($mota, $path);
+            if(!$check){
+                echo '<script>alert("Thêm danh mục thành công")</script>';
+                echo '<script>window.location.href="../admin/index.php?url=list-category"</script>';
+            }
         }
         include_once "Views/Category/add.php";
     }
@@ -36,16 +38,41 @@ class CategoryController
                 $path = $_FILES['path']['name'];
                 $target_dir = "uploads/";
                 $target_file = $target_dir . basename($_FILES["path"]["name"]);
-                move_uploaded_file($_FILES["path"]["tmp_name"], $target_file);
-                $this->category->suaCategory($id, $mota, $path);
-                echo '<script>alert("sửa thành công")</script>';
-                echo '<script>window.location.href="../admin/index.php?url=list-category"</script>';
-            } else {
+        
+                // Kiểm tra xem có dữ liệu trong $_FILES['path']['name'] không
+                if (!empty($path)) {
+                    // Kiểm tra và tạo thư mục lưu trữ nếu chưa tồn tại
+                    if (!is_dir($target_dir)) {
+                        mkdir($target_dir, 0755, true);
+                    }
+                    // Kiểm tra hình ảnh có hợp lệ không
+                    $check = getimagesize($_FILES['path']['tmp_name']);
+                    if ($check !== false) {
+                        if (move_uploaded_file($_FILES['path']['tmp_name'], $target_file)) {
+                            $this->category->suaCategory($id, $mota, $path);
+                            echo '<script>alert("Sửa thành công")</script>';
+                            echo '<script>window.location.href="../admin/index.php?url=list-category"</script>';
+                        } else {
+                            die("OOP !");
+                        }
+                    } else {
+                        $this->category->suaCategory2($id, $mota);
+                        echo '<script>alert("Sửa thành công")</script>';
+                        echo '<script>window.location.href="../admin/index.php?url=list-category"</script>';
+                    }
+                } else {
+                    $this->category->suaCategory2($id, $mota);
+                    echo '<script>alert("Sửa thành công")</script>';
+                    echo '<script>window.location.href="../admin/index.php?url=list-category"</script>';
+                }
+            }
+            else {
                 $id = $_GET['id'];
                 $hienthiCategory = $this->category->hienthiCategory($id);
                 require_once "Views/Category/edit.php";
             }
         }
+        
     }
 
     public function deleteCategory()
